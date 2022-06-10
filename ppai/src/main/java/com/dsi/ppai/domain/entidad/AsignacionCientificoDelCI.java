d5b9a8b138f93901dc6e600c95f5ba7ec1a48828
felipearias2024 <felipe2arias@hotmail.com>
2022-06-10 18:59:06 -0300

    ahora si levanta

--- ppai/src/main/java/com/dsi/ppai/domain/entidad/AsignacionCientificoDelCI.java
+++ ppai/src/main/java/com/dsi/ppai/domain/entidad/AsignacionCientificoDelCI.java
@@ -1,13 +1,7 @@
 package com.dsi.ppai.domain.entidad;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AsignacionCientificoDelCI {
 }
