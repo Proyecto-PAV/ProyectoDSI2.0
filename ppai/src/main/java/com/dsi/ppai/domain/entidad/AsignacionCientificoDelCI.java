7a1928bc1aee510284ef031e9adb75ba280096e9
Maner <nahuam00@gmail.com>
2022-06-10 20:15:36 -0300

    creacion de clases de Asignacion cientifico y cambio de estado de turno

--- ppai/src/main/java/com/dsi/ppai/domain/entidad/AsignacionCientificoDelCI.java
+++ ppai/src/main/java/com/dsi/ppai/domain/entidad/AsignacionCientificoDelCI.java
@@ -1,7 +1,37 @@
 package com.dsi.ppai.domain.entidad;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Builder
 public class AsignacionCientificoDelCI {
+    private Date fechaDesde;
+    private Date fechaHasta;
+    private String eMail;
+    private ArrayList<Turno> turnos;
+
+    public boolean esCientificoActivo(){
+        if (this.fechaHasta == null){
+            return true;
+        }
+        return false;
+    }
+
+    public void misTurnos(){
+    }
+
+    public void mostrarCientificoDelCl(){
+    }
+
+    public void setTurno(Turno turno){
+        this.turnos.add(turno);
+    }
 }
--- ppai/src/main/java/com/dsi/ppai/domain/entidad/CambioEstadoTurno.java
+++ ppai/src/main/java/com/dsi/ppai/domain/entidad/CambioEstadoTurno.java
@@ -1,7 +1,33 @@
 package com.dsi.ppai.domain.entidad;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Builder
 public class CambioEstadoTurno {
+    private Date fechaHoraDesde;
+    private Date fechaHoraHasta;
+    private Estado estado;
+
+    private void mostrarCambioEstadoTurno(){
+
+    }
+
+    public boolean esActual(){
+        if (this.fechaHoraHasta == null){
+            return true;
+        }
+        return false;
+    }
+
+    public String obtenerEstado(){
+        return this.estado.getNombre();
+    }
 }
