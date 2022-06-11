1df7481f41753e8296590c1c2e167a4b3df66e5b
Guille Gribaudo <80915763+GuilleTG20@users.noreply.github.com>
2022-06-10 21:09:37 -0300

    cambios sesion turno

--- ppai/src/main/java/com/dsi/ppai/domain/entidad/Turno.java
+++ ppai/src/main/java/com/dsi/ppai/domain/entidad/Turno.java
@@ -1,7 +1,88 @@
 package com.dsi.ppai.domain.entidad;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "turno")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 @Data
 public class Turno {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_turno")
+    private int idTurno;
+
+    @Column(name = "dia_semana")
+    private Date diaSemana;
+
+    @Column(name = "fecha_generacion")
+    private Date fechaGeneracion;
+
+    @Column(name = "fecha_hora_fin")
+    private Date fechaHoraFin;
+
+    @Column(name = "fecha_hora_inicio")
+    private Date fechaHoraInicio;
+
+    @OneToMany
+    @JoinColumn(name = "id_cambio_estado_turno")
+    private List<CambioEstadoTurno> cambiosEstadoTurno;
+
+    @Column(name = "estado_actual")
+    private Estado estadoActual;
+
+    @Column(name = "cambio_estado_actual")
+    private CambioEstadoTurno cambioEstadoActual;
+
+    public boolean estoyDisponible(){
+        if (fechaHoraFin == null) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public Date getFechaHoraInicio() {
+        return fechaHoraInicio;
+    }
+
+    public Date getFechaHoraFin() {
+        return fechaHoraFin;
+    }
+
+    public boolean esPosteriorFechaActual(Date fechaActual){
+        if (fechaActual.before(this.fechaHoraInicio)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public List mostrarDatos(){
+        List arrayDatos = new ArrayList();
+        for (int i = 0; i < this.cambiosEstadoTurno.size(); i++) {
+            if (cambiosEstadoTurno.get(i).esActual()){
+                this.cambioEstadoActual = cambiosEstadoTurno.get(i);
+            }
+        }
+        this.estadoActual = this.cambioEstadoActual.obtenerEstado();
+        arrayDatos.add(getFechaHoraInicio(), getFechaHoraFin(), this.estadoActual);
+        return arrayDatos;
+    }
+
+
 }
