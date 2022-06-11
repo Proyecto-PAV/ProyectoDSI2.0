fbd90978660c2ba34044540d05f2c2c5fe26d698
Joel Martinez <Joel.Martinez.ID@pinapp.pe>
2022-06-10 21:45:46 -0300

    fix

--- ppai/src/main/java/com/dsi/ppai/domain/entidad/CentroDeInvestigacion.java
+++ ppai/src/main/java/com/dsi/ppai/domain/entidad/CentroDeInvestigacion.java
@@ -1,61 +1,62 @@
 package com.dsi.ppai.domain.entidad;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.ReadOnlyProperty;
 
 import javax.persistence.*;
 import java.util.Date;
-import java.util.List;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder(toBuilder = true)
+@Builder
 @Data
 @Entity
-@Table(name = "centro_investigacion")
+@Table(name = "centro")
 public class CentroDeInvestigacion {
 
     //Atributos
-    @Column(name = "caracteristicas_generales")
-    private String caracteristicasGenerales;
-    @Column(name = "coordenadas")
-    private String coordenadas;
-    @Column(name = "correo_electronico")
-    private String correoElectronico;
-    @Column(name = "direccion")
-    private String direccion;
-    @Column(name = "edificio")
-    private String edificio;
-    @Column(name = "fecha_alta")
-    private Date fechaAlta;
-    @Column(name = "fecha_baja")
-    private Date fechaBaja;
-    @Column(name = "fecha_resolucion_creacion")
-    private Date fechaResolucionCreacion;
-    @Column(name = "motivo_Baja")
-    private String motivoBaja;
     @Id
-    @Column(name = "nombre_centro")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_centro")
+    private Integer id;
+
+    @Column(name="aracteristicas_generales")
+    private String caracteristicasGenerales;
+    @Column(name="coordenadas")
+    private String coordenadas;
+    @Column(name="correo_electronico")
+    private String correoElectronico;
+    @Column(name="direccion")
+    private String direccion;
+    @Column(name="edificio")
+    private String edificio;
+    @Column(name="fecha_alta")
+    private Date fechaAlta;
+    @Column(name="fecha_baja")
+    private Date fechaBaja;
+    @Column(name="fecha_resolucion_creacion")
+    private Date fechaResolucionCreacion;
+    @Column(name="motivo_Baja")
+    private String motivoBaja;
+    @Column(name="nombre_centro")
     private String nombre;
-    @Column(name = "numeor_resolucion_creacion")
+    @Column(name="numeor_resolucion_creacion")
     private int numeroResolucionCreacion;
-    @Column(name = "piso")
+    @Column(name="piso")
     private String piso;
-    @Column(name = "reglamento")
+    @Column(name="reglamento")
     private String reglamento;
-    @Column(name = "sigla")
+    @Column(name="sigla")
     private String sigla;
-    @Column(name = "telefono_contacto")
+    @Column(name="telefono_contacto")
     private String telefonoContacto;
-    @Column(name = "tiempo_antelacion_reserva")
+    @Column(name="tiempo_antelacion_reserva")
     private Date tiempoAntelacionReserva;
-    private List<AsignacionCientificoDelCI> cientificos;
-    private List<RecursoTecnologico> recursosTecnologicos;
+    //private List<AsignacionCientificoDelCI> cientificos;
+    //private List<RecursoTecnologico> recursosTecnologicos;
 
 
     //Métodos.
