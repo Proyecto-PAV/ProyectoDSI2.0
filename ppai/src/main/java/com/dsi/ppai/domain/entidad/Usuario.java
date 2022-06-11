ae67e6566dab5a3644310e0430b76be70763af00
felipearias2024 <felipe2arias@hotmail.com>
2022-06-10 21:57:01 -0300

    Clase Pantalla y Usuario

--- ppai/src/main/java/com/dsi/ppai/domain/entidad/Usuario.java
+++ ppai/src/main/java/com/dsi/ppai/domain/entidad/Usuario.java
@@ -1,7 +1,37 @@
 package com.dsi.ppai.domain.entidad;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Usuario {
+    @Id
+    private String usuario;
+    private String clave;
+    private Boolean habilitado;
+    @OneToOne
+    @JoinColumn(name = "empleado_sesion")
+    private Sesion sesion;
+
+
+    public void habilitar(){
+        this.habilitado = true;
+    }
+
+    public void inhabilitar(){
+        this.habilitado = false;
+    }
+
+    public void modificarPassword(){}
+
+    public void obtenerCientifico(){}
+
 }
